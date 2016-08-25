46ca98dd38b50108fff8ae8f6b1ea60639d91251
pchisholm <chisholm.p@gmail.com>
2016-08-25 12:27:49 -0300

    Added: Server side resizing re-implemented, error catching on mimtype added to post,  generic defaultfor error handling on express, missing modules from renewing the git

--- servtest.js
+++ servtest.js
@@ -7,13 +7,10 @@ var watson = require('watson-developer-cloud');
 var path = require('path')
 var crypto = require('crypto');
 var fs = require('fs');
-
-var languageTranslatorV2 = require('watson-developer-cloud/language-translator/v2');
-
-
-var sharp = require('sharp');
+var languageTranslatorV2 = require(
+  'watson-developer-cloud/language-translator/v2');
+var jimp = require('jimp');
 require("./public/js/resize.js");
-
 //Server var / Global
 var app = express();
 
@@ -21,45 +18,42 @@ var app = express();
 		Init Server, Set path
    ================================ */
 //Set port to 3000
-app.set('port' ,  3000);
+app.set('port', 3000);
 //Define the default server path to /public
 app.use(express.static(__dirname + '/public'));
+//Generic error handling middle-ware, which passes uncaught exceptions to the default Node error handler
+app.use(errorHandler);
 //Listen to port 3000, and process environment
 app.listen(process.env.PORT || 3000, function() {
-	console.log("Hosted on port 3000. Ctrl + c to close the server");
+  console.log("Hosted on port 3000. Ctrl + c to close the server");
 });
 
+function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render('error', { error: err });
+}//End errorHandler
 
 /* ================================
 			Multer Module
    ================================ */
-
 //Multer settings for images
 var storage = multer.diskStorage({
-  destination: './uploads/',
-  filename: function(req, file, cb) {
-    crypto.pseudoRandomBytes(16, function(err, raw) {
-      if (err) return cb(err)
-      cb(null, raw.toString('hex') + path.extname(file.originalname))
-    })
-  }
-})
-var fields = [{
-  name: 'userPhoto',
-  maxCount: 1
-}, {
-  name: 'positive',
-  maxCount: 1
-}, {
-  name: 'negative',
-  maxCount: 1
-}]
-
+    destination: './uploads/',
+    filename: function(req, file, cb) {
+      crypto.pseudoRandomBytes(16, function(err, raw) {
+        if (err) return cb(err)
+        cb(null, raw.toString('hex') + path.extname(file.originalname))
+      })
+    }
+  })
+  
 //Store Multer results
 var upload = multer({
   storage: storage
-}).fields(fields);
-
+}).single('userPhoto');
 
 /* ================================
 		Watson API Credentials
@@ -70,7 +64,6 @@ var visual_recognition = watson.visual_recognition({
   version: 'v3',
   version_date: '2016-05-19'
 });
-
 var language_translator = new languageTranslatorV2({
   username: "c5393096-827e-49f8-961f-36120c6cda8a",
   password: "Igiv7HTwPRCD"
@@ -79,7 +72,6 @@ var language_translator = new languageTranslatorV2({
 /* ================================
 		Classifiers
    ================================ */
-
 app.get('/test', function(req, res) {
   // var params = {
   // name: 'poodle', 
@@ -103,58 +95,75 @@ app.get('/test', function(req, res) {
 /* ================================
     GET data from Client
    ================================ */
-
-
+   
 /* ================================
 		Post to Multer API
    ================================ */
-
 app.post('/api/photo', function(req, res) {
-
   upload(req, res, function(err) {
-    if (err) {
-      return res.end("Error uploading file.");
+    if (!req.file.mimetype.match(/\.(jpg|jpeg|png|gif)$/)) {
+      res.end("This application does not support that file extension.");
     }
-    console.log(req.files['userPhoto']);
-    var params = {
-      images_file: fs.createReadStream(req.files['userPhoto'][0].path) // must be a .zip file containing images
-    };
-
-    visual_recognition.recognizeText(params, function(err, resRecognizeText) {
-      if (err) {
-        console.log(err);
-      }
-      else {
-        var outText = "";
-        var json = JSON.parse(JSON.stringify(resRecognizeText, null, 2));
-        for (var item in json.images) {
-          for (var index in json.images[item].words) {
-            outText += json.images[item].words[index].word + " ";
+    if (err) {
+      res.end("Error uploading file.");
+    }
+	
+    var filename = req.file.destination + Date.now() + "resized.png";
+    jimp.read(req.file.path, function(err, file) {
+      file.resize(200, 200).quality(100).write(filename, function(
+        err) {
+        if (err) {
+          console.log(err);
+        } else {
+          analyzeText(filename);
+          fs.unlinkSync(req.file.path);
+        } //End else
+      }); //End write function
+    }); //End read function
+    //console.log(req.file);
+  }); //End upload function
+  
+  function analyzeText(file) {
+      var params = {
+        images_file: fs.createReadStream(file) // must be a .zip file containing images
+      };
+      visual_recognition.recognizeText(params, function(err,
+        resRecognizeText) {
+        if (err) {
+          console.log(err);
+          res.end(
+            "Watson is experiencing difficulties... Please try again."
+          )
+        } else {
+          var outText = "";
+          var json = JSON.parse(JSON.stringify(resRecognizeText, null,
+            2));
+          for (var item in json.images) {
+            for (var index in json.images[item].words) {
+              outText += json.images[item].words[index].word + " ";
+            }
           }
-        } 
-        translate(outText);
-      }   
-    }); 
-  });
+          translate(outText);
+        }
+      });
+    } //End analyzeTest
 
- function translate(outText) {
-    language_translator.translate({
-      text: outText, 
-      source : 'en', 
-      target: 'es' 
-    },
-      function (err, translation) {
-        if (err)
-          console.log('error:', err);
+  function translate(outText) {
+      language_translator.translate({
+        text: outText,
+        source: 'en',
+        target: 'es'
+      }, function(err, translation) {
+        if (err) console.log('error:', err);
         else {
           var translatedText = "";
-          var jsonTranslated = JSON.parse(JSON.stringify(translation, null, 2));
+          var jsonTranslated = JSON.parse(JSON.stringify(translation,
+            null, 2));
           for (var key in jsonTranslated.translations) {
             translatedText += jsonTranslated.translations[key].translation;
           }
-         res.end(translatedText);
-      }
-    });
-  }
-});
-
+          res.end(translatedText);
+        }
+      });
+    } //End translate
+}); //End post
